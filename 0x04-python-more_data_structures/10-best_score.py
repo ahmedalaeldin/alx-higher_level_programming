f0749e964ecc2dfd27f93b08605bb22b4e022194
ahmedalaeldin <alaa.mido515@yahoo.com>
2024-01-10 13:28:41 -0800

    new updlaed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    mxval = 0
+    maxval = 0
     maxkey = None
     for k, v is a_dictionary.items():
-        if v > mxval:
+        if v > maxval:
             maxval = v
             maxkey = k
     return maxkey
